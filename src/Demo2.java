b26cecf6e91f8f4beb876802d3aee37fb57ef07a
Ariazm0 <1106030429@qq.com>
2020-08-04 22:20:42 +0800

    turn

--- src/Demo2.java
+++ src/Demo2.java
@@ -6,4 +6,13 @@
  * Time: 21:47
  */
 public class Demo2 {
+    public static void turn(ListNode head) {
+        
+    }
+
+
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        ListNode node = demo1.build();
+    }
 }
